dce197b897c3cc4f549c4cc8ba26ff916931f14d
leechanghwanspace <spacecehh@naver.com>
2024-09-17 23:13:55 +0900

    [sec6] Spring MVC ✅

--- DEEP DIVE_SPRING BOOT/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ DEEP DIVE_SPRING BOOT/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan	//서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +14,12 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+//	직접 할 필요 없이 아래 내용들은 직접 할 필요 없이 스프링부트에서 알아서 다 해줌.
+
+//	@Bean
+//	InternalResourceViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	}
 
 }
