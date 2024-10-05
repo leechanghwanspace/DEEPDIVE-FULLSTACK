cefe3f59c1e56f1dc283d7e78f777fce31454740
leechanghwanspace <spacecehh@naver.com>
2024-10-05 18:40:05 +0900

    [10. 응답/에러모델 과제 생성]

---
+++ DEEP DIVE_assignment/ResponseErrorModel/src/main/java/com/project/_0/Response/Error_Model/Application.java
@@ -0,0 +1,13 @@
+package com.project._0.Response.Error_Model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ DEEP DIVE_assignment/ResponseErrorModel/src/test/java/com/project/_0/Response/Error_Model/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project._0.Response.Error_Model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
