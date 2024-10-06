35320de7c171f43a5a6a1d2e820732ca9a9cbdcf
leechanghwanspace <spacecehh@naver.com>
2024-10-07 01:47:23 +0900

    [sec11] Spring Type Converter

---
+++ DEEP DIVE_SPRING BOOT/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+//        컨버터가 우선순위가 높기 때문에 타입이 같을시 포맷터 적용 안되므로 주석 처리
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+
+        //포맷터 추가
+        registry.addFormatter(new MyNumberFormatter());
+    }
+}
